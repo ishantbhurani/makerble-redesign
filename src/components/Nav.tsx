68e811a53745b818b58553877e30bbc7c8d470f7
Ishant Bhurani <67356291+ishantbhurani@users.noreply.github.com>
2024-10-22 20:32:54 +0530

    make nav and sidebar sticky on scroll

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,37 +5,42 @@ import NavLink from './NavLink'
 
 export default function Nav() {
   return (
-    <nav className='row-span-full flex flex-col border-r border-gray-200'>
-      <div className='mx-auto p-3 md:mx-0'>
-        <Link to='/'>
-          <picture>
-            <source srcSet='/logo.png' media='(min-width: 768px)' />
-            <img
-              src='/logo-mobile.png'
-              alt='Makerble'
-              width={134}
-              height={38}
-              className='block h-[38px] w-auto max-w-full'
-            />
-          </picture>
-        </Link>
-      </div>
+    <nav className='row-span-full border-r border-gray-200'>
+      <div className='sticky top-0 flex min-h-screen flex-col'>
+        <div className='mx-auto p-3 md:mx-0'>
+          <Link to='/'>
+            <picture>
+              <source srcSet='/logo.png' media='(min-width: 768px)' />
+              <img
+                src='/logo-mobile.png'
+                alt='Makerble'
+                width={134}
+                height={38}
+                className='block h-[38px] w-auto max-w-full'
+              />
+            </picture>
+          </Link>
+        </div>
 
-      <ul>
-        {NAV_LINKS.map((navLink, index) => (
-          <li key={index}>
-            <NavLink {...navLink} />
-          </li>
-        ))}
-      </ul>
+        <ul>
+          {NAV_LINKS.map((navLink, index) => (
+            <li key={index}>
+              <NavLink
+                {...navLink}
+                className={index === 0 ? 'bg-[rgba(30,64,175,0.15)]' : ''} // simulating active link
+              />
+            </li>
+          ))}
+        </ul>
 
-      <div className='mt-auto'>
-        <NavLink
-          href='/'
-          icon={<Settings />}
-          label='Settings'
-          className='p-4'
-        />
+        <div className='mt-auto'>
+          <NavLink
+            href='/'
+            icon={<Settings />}
+            label='Settings'
+            className='p-4'
+          />
+        </div>
       </div>
     </nav>
   )
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,8 +4,10 @@ import UpcomingTasks from './UpcomingTasks'
 export default function Sidebar() {
   return (
     <aside className='hidden border-l border-gray-200 lg:block'>
-      <PersonalProgress />
-      <UpcomingTasks />
+      <div className='sticky top-6'>
+        <PersonalProgress />
+        <UpcomingTasks />
+      </div>
     </aside>
   )
 }
